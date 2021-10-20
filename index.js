327cd0b185818e2efdd3d958933d727887e72234
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-10-20 18:34:55 -0300

    começo da função p/ leitura dos diretorios

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-module.exports = () => {
-  // ...
-};
+// const chalk = require('chalk');
+const fs = require('fs');
+
+function mdLinks(paths, options) {
+
+  fs.readFile(file, 'utf8', (err, data) => {
+
+    return new Promise((resolve, reject) => {
+      const regex = /[\s\S]+(http[s]?[^)]*)/gm;
+      const batata = data.match(regex);
+
+      if (err) {
+        reject(err);
+      } else {
+        console.log(batata)
+      }
+    })
+  })
+}
+
+
+module.exports = mdLinks();
